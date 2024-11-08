536efe9d585ccd3411f9ca23b0b888f11375fe60
yerin <dev.yerin@gmail.com>
2024-11-08 16:21:00 +0900

    chore: env example 주석 추가

--- src/config/metadata.ts
+++ src/config/metadata.ts
@@ -5,7 +5,7 @@ import { SiteConfig } from '@/types';
 -- Fill the details about your website
  ==================== */
 
-export const baseUrl = process.env.BACKEND_API_URL || '';
+export const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || '';
 
 export const siteConfig: SiteConfig = {
   name: 'PrezWiz',
